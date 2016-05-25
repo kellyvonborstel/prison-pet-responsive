b1ea4a40e08c405fcc7b59ededcf453594eb2c00
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-25 15:26:01 -0700

    add slide menu for small screens

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-// added click handler to prevent images from disappearing on ios
 
 $(function() {
 
+  // slide menu for small screens
+
+  $('.nav-menu-open').on('click', function(e) {
+    e.preventDefault();
+    $('body').addClass('menu-open');
+    $('.nav-menu').addClass('active');
+    $('.mask').addClass('active');
+    $('.nav-menu-open').disabled = true;
+  });
+
+  $('.nav-menu-close').on('click', function(e) {
+    e.preventDefault();
+    $('body').removeClass('menu-open');
+    $('.nav-menu').removeClass('active');
+    $('.mask').removeClass('active');
+    $('.nav-menu-open').disabled = false;
+  });
+
+  // added click handlers to prevent images from disappearing on ios
+  
   $('.js-donate-home').on({
     mouseenter: function() {
       $('.js-donate-home').attr('src', 'images/donate-blue-dark.svg');
@@ -194,141 +213,4 @@ $(function() {
     }
   });
 
-});
-
-
-(function(window) {
-
-  'use strict';
-
-  /**
-   * Extend Object helper function.
-   */
-  function extend(a, b) {
-    for(var key in b) { 
-      if(b.hasOwnProperty(key)) {
-        a[key] = b[key];
-      }
-    }
-    return a;
-  }
-
-  /**
-   * Each helper function.
-   */
-  function each(collection, callback) {
-    for (var i = 0; i < collection.length; i++) {
-      var item = collection[i];
-      callback(item);
-    }
-  }
-
-  /**
-   * Menu Constructor.
-   */
-  function Menu(options) {
-    this.options = extend({}, this.options);
-    extend(this.options, options);
-    this._init();
-  }
-
-  /**
-   * Menu Options.
-   */
-  Menu.prototype.options = {
-    wrapper: '#o-wrapper',          // The content wrapper
-    type: 'slide-left',             // The menu type
-    menuOpenerClass: '.button',   // The menu opener class names (i.e. the buttons)
-    maskId: '#mask'               // The ID of the mask
-  };
-
-  /**
-   * Initialise Menu.
-   */
-  Menu.prototype._init = function() {
-    this.body = document.body;
-    this.wrapper = document.querySelector(this.options.wrapper);
-    this.mask = document.querySelector(this.options.maskId);
-    this.menu = document.querySelector('#menu-' + this.options.type);
-    this.closeBtn = this.menu.querySelector('.menu-close');
-    this.menuOpeners = document.querySelectorAll(this.options.menuOpenerClass);
-    this._initEvents();
-  };
-
-  /**
-   * Initialise Menu Events.
-   */
-  Menu.prototype._initEvents = function() {
-    // Event for clicks on the close button inside the menu.
-    this.closeBtn.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.close();
-    }.bind(this));
-
-    // Event for clicks on the mask.
-    this.mask.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.close();
-    }.bind(this));
-  };
-
-  /**
-   * Open Menu.
-   */
-  Menu.prototype.open = function() {
-    this.body.classList.add('has-active-menu');
-    this.wrapper.classList.add('has-' + this.options.type);
-    this.menu.classList.add('is-active');
-    this.mask.classList.add('is-active');
-    this.disableMenuOpeners();
-  };
-
-  /**
-   * Close Menu.
-   */
-  Menu.prototype.close = function() {
-    this.body.classList.remove('has-active-menu');
-    this.wrapper.classList.remove('has-' + this.options.type);
-    this.menu.classList.remove('is-active');
-    this.mask.classList.remove('is-active');
-    this.enableMenuOpeners();
-  };
-
-  /**
-   * Disable Menu Openers.
-   */
-  Menu.prototype.disableMenuOpeners = function() {
-    each(this.menuOpeners, function(item) {
-      item.disabled = true;
-    });
-  };
-
-  /**
-   * Enable Menu Openers.
-   */
-  Menu.prototype.enableMenuOpeners = function() {
-    each(this.menuOpeners, function(item) {
-      item.disabled = false;
-    });
-  };
-
-  /**
-   * Add to global namespace.
-   */
-  window.Menu = Menu;
-
-})(window);
-
-var slideRight = new Menu({
-  wrapper: '#o-wrapper',
-  type: 'slide-right',
-  menuOpenerClass: '.button',
-  maskId: '#mask'
-});
-
-var slideRightBtn = document.querySelector('#button-slide-right');
-
-slideRightBtn.addEventListener('click', function(e) {
-  e.preventDefault();
-  slideRight.open();
 });
